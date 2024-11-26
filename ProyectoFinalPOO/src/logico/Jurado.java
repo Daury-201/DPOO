37963af089aeecf4d366ab5dabb7e410c66fe4b2
Adelso Jimenez <Dell Inspiron 15@DESKTOP-L2MIQPQ>
2024-11-25 20:58:34 -0400

    Cambios
    Se añadieron metodos de agregar/remover jurados y trabajo en comision y
    se agregaron metodos de agregar/remover comisiones en jurado

--- ProyectoFinalPOO/src/logico/Jurado.java
+++ ProyectoFinalPOO/src/logico/Jurado.java
@@ -1,9 +1,25 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Jurado extends Persona {
 
-	public Jurado(String id, String nombre, String apellido, String telefono, String direccion) {
-		super(id, nombre, apellido, telefono, direccion);
-	}
+    private ArrayList<Comision> comisiones;
 
+    public Jurado(String id, String nombre, String apellido, String telefono, String direccion) {
+        super(id, nombre, apellido, telefono, direccion);
+        this.comisiones = new ArrayList<>();
+    }
+
+    public ArrayList<Comision> getComisiones() {
+        return comisiones;
+    }
+
+    public void agregarComision(Comision comision) {
+        comisiones.add(comision);
+    }
+
+    public void removerComision(Comision comision) {
+        comisiones.remove(comision);
+    }
 }
